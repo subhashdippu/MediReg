327e04f9f988b4cde91b9547a0520ae305fedf07
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-05-25 17:34:08 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ["@electric-sql/pglite"],
+  },
+  build: {
+    target: "esnext",
+  },
+  assetsInclude: ["**/*.wasm"],
+});
